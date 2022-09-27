d1ccf1430273a127584f2d014503093bd8b3247d
Marcos Andrade <marcos_q_andrade@hotmail.com>
2022-09-27 16:04:37 -0300

    Navbar Stylling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,11 +1,14 @@
-import './assets/styles/custom.scss'
+import Navbar from './components/Navbar';
+import './assets/styles/custom.scss';
 import './App.css';
 
-
 function App() {
+
   return (
-    <h1 >Hello DSCatalog</h1>
-  );
+  <>
+  <Navbar/>
+  <h1>Hello DSCatalog</h1>
+  </>);
 }
 
 export default App;
